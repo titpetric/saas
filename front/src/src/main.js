ffdc617090e1e2f6f0e6cd76630b507c4f00aba2
Tit Petric <black@scene-si.org>
2017-08-17 15:21:59 +0200

    add globals

--- front/src/src/main.js
+++ front/src/src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import './global'
+import './css/index.scss'
 
 /* eslint-disable no-new */
 new Vue({
